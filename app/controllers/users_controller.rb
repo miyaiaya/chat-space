4ff34ec41a8f4aaff953ac1e6721a66f0361c27c
宮井文 <westtaila42240@gmail.com>
2018-09-11 19:10:53 +0900

    edit users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,19 @@
 class UsersController < ApplicationController
 
-def edit
+  def edit
+  end
 
-end
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
 
-def update
+  private
 
-
-end
-
-
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
