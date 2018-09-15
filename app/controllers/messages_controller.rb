487c97c89fddc0477a2d62712638453c03c7d126
宮井文 <westtaila42240@gmail.com>
2018-09-15 17:45:07 +0900

    edit message_controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,25 @@
 class MessagesController < ApplicationController
+  before_action :set_group
 
  def index
 
  end
 
  def create
+  @message = Message.create(message_params)
+    if @message.save
+      redirect_to root_path
+    else
+      notice 'メッセージを入力してください。'
+    end
+ end
 
+ private
+ def message_params
+  params.require(:message).permit(:body, :image)
+ end
+
+ def set_group
+   @group = Group.find(params[:group_id])
  end
 end
