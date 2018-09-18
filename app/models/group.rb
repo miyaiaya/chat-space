c878d873f1f1fb6dba41ca4f7da8800043df0ee8
宮井文 <westtaila42240@gmail.com>
2018-09-18 18:56:14 +0900

    edit group model (add show_last_message)

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,14 @@
 class Group < ApplicationRecord
- has_many :members
- has_many :users, through: :members
- validates :name, presence: true
- has_many :messages
+   has_many :members
+   has_many :users, through: :members
+   validates :name, presence: true
+   has_many :messages
+
+   def show_last_message
+    if (last_message = messages.last).present?
+     last_message.content? ? last_message.content
+    else
+     'まだメッセージはありません。'
+    end
+   end
 end
