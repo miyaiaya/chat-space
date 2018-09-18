c6141dddd0a40bd15920d923a6ce3b095d4af835
宮井文 <westtaila42240@gmail.com>
2018-09-18 19:16:00 +0900

    show last message on side bar

--- app/models/group.rb
+++ app/models/group.rb
@@ -6,7 +6,7 @@ class Group < ApplicationRecord
 
    def show_last_message
     if (last_message = messages.last).present?
-     last_message.content? ? last_message.content
+     last_message.body? ? last_message.body : '画像が投稿されました'
     else
      'まだメッセージはありません。'
     end
