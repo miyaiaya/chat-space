4266f348974b2470a91b0704b4ea1e8f8dc58b09
宮井文 <westtaila42240@gmail.com>
2018-09-29 19:27:54 +0900

    add $.ajax

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,5 +2,14 @@ $(function(){
   $('.form').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
+
+    $.ajax({
+      url: '/messages/create',
+      type: 'POST',
+      data: formData,
+      dataType: 'json'
+      processData: false,
+      contentType: false
+    })
   });
 });
