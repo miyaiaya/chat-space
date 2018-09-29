01999134fe4e5d20cc0ae8504f4d7be4db396cdf
宮井文 <westtaila42240@gmail.com>
2018-09-29 21:44:08 +0900

    add buildHTML & done

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,22 @@
 $(function(){
+  function buildHTML(message){
+    image = message.image? `<img src="{message.image} class='lower-message__image'>`
+    var html =`<div class="message-content">
+               <h5 class="message-content__name">
+                 ${message.user.name}
+               </h5>
+               <span class= "message-content__date">
+                 ${message.created_at}
+               </div>
+               <div class="lower-message">
+                 <p class="message-content__text">
+                   ${message.body}
+                 </p>
+                   ${image}
+               </div>`
+    return html;
+  }
+
   $('.form').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -10,6 +28,11 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(){
+      var html = buildHTML(data);
+      $('.message-content__text').append(html)
+      $('.form__form-area').val()
     })
   });
 });
