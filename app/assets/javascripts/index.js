fbca7d1ed5d3988e6b182fe4b81f9c79daf98053
宮井文 <westtaila42240@gmail.com>
2018-10-02 23:27:04 +0900

    add html

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,4 +1,11 @@
 $(function(){
+  function appendUser(user){
+    var html =`<div class="chat-group-user clearfix">
+                 <p class="chat-group-user__name">ユーザー名</p>
+                 <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="ユーザーのid" data-user-name="ユーザー名">追加</a>
+              </div>`
+  }
+
   $('#user-search-field').on("keyup", function(){
     var input =$('#user-search-field').val();
     console.log(input);
