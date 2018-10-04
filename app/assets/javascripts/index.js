e92ae03133149fa0d83cecd4e2ff80223732210e
宮井文 <westtaila42240@gmail.com>
2018-10-04 21:32:15 +0900

    add group-user function

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -10,6 +10,16 @@ $(function(){
     search_list.append(html);
   }
 
+  var group_user_list = $("#chat-group-users");
+
+  function appendGroupUser(user_id, user_name){
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+                  <input name='group[user_ids][]' type='hidden' value='${user_id}'>
+                  <p class='chat-group-user__name'>${user_name}</p>
+                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                </div>`
+    group_user_list.append(html);
+  }
 
   $('#user-search-field').on("keyup", function(){
     var input =$('#user-search-field').val();
@@ -32,7 +42,11 @@ $(function(){
     alert('ユーザー検索に失敗しました');
   })
   });
-  $(document).on('click', '.user-search-add', function(){
+  $(document).on('click', '.chat-group-user__btn--add', function(){
+    var user_id = $(this).attr('data-user-id');
+    var user_name = $(this).attr('data-user-name');
+    appendGroupUser(user_id, user_name);
+    $(this).parent().remove();
 
 
   });
